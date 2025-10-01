788d3c76ac9139f206ab304ac24dbc34ec2765eb
Manish Karautia <manishkarautia@gmail.com>
2025-10-01 15:13:13 +0530

    app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_text_chunks(text):
     return chunks
 
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
